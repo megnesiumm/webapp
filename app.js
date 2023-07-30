b49663d8d5dc8468c1b9fd010597a646d4b761de
megnesiumm <pongsakorn.spt1@gmail.com>
2023-07-30 18:35:07 +0700

    web2

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const debug = require('debug')('app');
-
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "/pubilc/")));
 app.get("/", (req, res) => {
 
-    res.send('Hello World555');
+    res.send('Hello foon');
 
 })
 
 app.listen(port, () => {
-    debug("Listening on port ",port);
+    debug("Listening on port ", port);
 })
